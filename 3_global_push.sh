9741296c46004fd1c937eaaf9440328cbf60e209
Sergey Maksimov <sergeymaksimov1993@gmail.com>
2021-10-09 14:11:07 +0300

    autoupdate 2021-10-09-14:11:07

--- 3_global_push.sh
+++ 3_global_push.sh
@@ -2,35 +2,29 @@
 echo -en "\n"
 echo "global push - start"
 echo -en "\n"
-
 cd..
 
 # db
-
 cd db
 ./1_local_push.sh
 cd ..
 
 # codegym_java
-
 cd codegym_java
 ./1_local_push.sh
 cd ..
 
 # spring-cloud-client-example
-
 cd spring-cloud-client-example
 ./1_local_push.sh
 cd ..
  
-# spring-cloud-client-example
-
-cd spring-cloud-client-example 
+# spring-cloud-server-example
+cd spring-cloud-server-example 
 ./1_local_push.sh
 cd ..
 
 # dotfiles
-
 cd dotfiles
 ./1_local_push.sh
 cd ..
